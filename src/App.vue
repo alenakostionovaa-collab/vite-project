24fb6a9ed7517ae1e3e116173f2a632038e845a6
caelykk <caelykkaty.m@gmail.com>
2026-03-26 12:16:14 +0300

    добавили локал сторэдж чекбоксы и  поменяли сохранение как объекты до этого хранились строки

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,59 @@
   <div class="wrapper">
   <input v-model="newTodo"
   placeholder="Запиши задачу"
-  @keyup.enter="addTodo"/> <!--чтобы не переходя на кнопку  -->
+  @keyup.enter="addTodo"/>
 
   <button class="button" @click="addTodo">отправить</button>
   </div>
+
   <ul>
-    <li v-for="(todo, index ) in todos" :key="index">
-{{ todo }}
+
+    <li   v-for="(todo, index ) in todos" :key="todo.id">
+      <input :style="{'accent-color': 'cadetblue'}" id="checkbox" type="checkbox" v-model="todo.completed"/>
+      <span :style="{textDecoration:todo.completed ? 'line-through': 'none'}">
+       {{ todo.text }}
+
+</span>
+
 <button @click="removeTodo(index)">x</button>
+
+<!--<div :style="{ width: '100%', height: '2px', backgroundColor: 'gray' }"></div>-->
     </li>
+
   </ul>
+
   </div>
+
+
 </template>
 
 <script setup>
-import { remove } from '@vue/shared';
-import {ref} from 'vue'
+
+import {ref,watch} from 'vue'
 const newTodo = ref ('')
 const todos = ref ([])
 function addTodo(){
 if( newTodo.value.trim())
-todos.value.push(newTodo.value)
+todos.value.push({
+  id:Date.now(),
+  text:newTodo.value,
+  completed:false
+})
 newTodo.value=''//очистили
 }
 
 function removeTodo(index){
 todos.value.splice(index,1)
 }
+const saved = localStorage.getItem('todos')
+if (saved){
+  todos.value=JSON.parse(saved)
+}
+watch(todos,(newTodos)=>{
+  localStorage.setItem('todos',JSON.stringify(newTodos))
+},{deep:true})
+
+
 </script>
 <style scoped>
 *{
@@ -56,5 +82,19 @@ input{
   height: 25px;
  font-size: 15px;
 }
+.checkbox{
+color: cadetblue;
+}
+li{
+display: flex;
+justify-content: center;
+box-sizing: border-box;
+gap: 5px;
 
+}
+.todo-item {
+  display: flex;
+  flex-direction: column; /* чтобы контент и линия были сверху/снизу */
+  width: 100%;
+}
 </style>
